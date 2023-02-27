643bbe6e39ec35c5706da62934dac2295b1880a7
manishkarki358 <rajkarki358358@gmail.com>
2023-02-27 21:07:04 +0545

    initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// getting DOM elements
+const myName=document.querySelector('.name')
+const starting=document.querySelector('.starting')
+const profession=document.querySelector('.profession')
+
+// changing them into array
+ let nameArray=myName.innerText.split('')
+ let startingArray=starting.innerText.split('')
+ let professionArray=profession.innerText.split('')
+ console.log(nameArray);
+ myName.innerText=""
+//console.log(myName,starting,profession);
+nameArray.forEach(element => {
+    element=element.replace(/"/g,'')
+    myName.innerHTML+=`<span class="spanning">${element}<span>`
+   
+    });
+    const gettingName=Array.from(document.querySelectorAll('.spanning'))
+    gettingName.forEach(element=>{
+        element.addEventListener('mouseover',()=>{
+            element.classList.toggle('animating')
+            element.classList.toggle('fa-shake')
+                const gettingName=Array.from(document.querySelectorAll('.spanning'))
+                gettingName.forEach(element=>{
+                    element.classList.remove('fa-shake')
+                })
+                element.classList.toggle('fa-shake') 
+            
+          
+            
+            //console.log(element.innerText);
+            //element.classList.add('fa-shake')
+        })
+    })
+    console.log(gettingName);
+ 
